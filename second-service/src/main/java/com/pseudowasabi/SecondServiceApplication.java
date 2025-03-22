719a76f1b052ce2b54d83cab5e3e047ce71d0a73
pseudowasabi <pseudowasabi@kakao.com>
2025-03-22 18:11:07 +0900

    (first-service, second-service) convert to spring application

--- second-service/src/main/java/com/pseudowasabi/SecondServiceApplication.java
+++ second-service/src/main/java/com/pseudowasabi/SecondServiceApplication.java
@@ -1,10 +1,11 @@
 package com.pseudowasabi;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecondServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(SecondServiceApplication.class, args);
     }
 }
