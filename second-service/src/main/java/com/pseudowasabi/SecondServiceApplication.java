7baf6d8994f93414d08e94c85755a886608b058f
pseudowasabi <pseudowasabi@kakao.com>
2025-03-22 18:08:56 +0900

    add modules [first-service], [second-service]

---
+++ second-service/src/main/java/com/pseudowasabi/SecondServiceApplication.java
@@ -0,0 +1,10 @@
+package com.pseudowasabi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
