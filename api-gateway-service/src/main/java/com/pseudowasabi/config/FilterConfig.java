f1d6c5e9b0222c34790977ef932df2ef5fc95e79
Yoseob Kim <pseudowasabi@kakao.com>
2025-05-25 20:55:56 +0900

    (gateway) modify filter response header

--- api-gateway-service/src/main/java/com/pseudowasabi/config/FilterConfig.java
+++ api-gateway-service/src/main/java/com/pseudowasabi/config/FilterConfig.java
@@ -13,12 +13,12 @@ public class FilterConfig {
     RouteLocator customGatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(r -> r.path("/first-service/**")
-                        .filters(f -> f.addRequestHeader("first-req-header", "first-req-header-value")
-                                .addResponseHeader("first-resp-header", "first-resp-header-value"))
+                        .filters(f -> f.addRequestHeader("first-req-header", "first-req-header-value1")
+                                .addResponseHeader("first-resp-header", "first-resp-header-value1"))
                         .uri("http://127.0.0.1:8081"))
                 .route(r -> r.path("/second-service/**")
-                        .filters(f -> f.addRequestHeader("second-req-header", "second-req-header-value")
-                                .addResponseHeader("second-resp-header", "second-resp-header-value"))
+                        .filters(f -> f.addRequestHeader("second-req-header", "second-req-header-value1")
+                                .addResponseHeader("second-resp-header", "second-resp-header-value1"))
                         .uri("http://127.0.0.1:8082"))
                 .build();
     }
