e1db5c984355b11956fe4f664f546397e8396e64
Yoseob Kim <pseudowasabi@kakao.com>
2025-06-07 15:37:15 +0900

    (first-service) run application on random port

--- first-service/src/main/java/com/pseudowasabi/FirstServiceController.java
+++ first-service/src/main/java/com/pseudowasabi/FirstServiceController.java
@@ -1,19 +1,31 @@
 package com.pseudowasabi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
 
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
-    public String welcome(@RequestHeader("first-req-header") String firstRequestHeader) {
-        log.info("Header value: {}", firstRequestHeader);
-        return "Welcome to first-service.";
+    public String welcome(@RequestHeader("first-req-header") String firstRequestHeader,
+                          HttpServletRequest httpServletRequest) {
+        log.info("Server port: {}, Header value: {}", httpServletRequest.getServerPort(), firstRequestHeader);
+        return String.format("Welcome to first-service on port %s", env.getProperty("local.server.port"));
     }
 }
